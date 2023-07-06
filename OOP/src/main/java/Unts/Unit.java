507b9689b41f5580d773f0ab5dcde78341e0a7de
Mutniy <wrobotw@mail.ru>
2023-07-06 22:35:02 +0300

    добавили в абстрактный класс метод для поиска ближайшего unita

--- OOP/src/main/java/Unts/Unit.java
+++ OOP/src/main/java/Unts/Unit.java
@@ -1,6 +1,8 @@
 package Unts;
 
 
+import java.util.ArrayList;
+
 public abstract class Unit implements GameInterface {
     protected String name;
 
@@ -13,9 +15,10 @@ public abstract class Unit implements GameInterface {
     protected int point;/// очки действий
     protected  int position;
 
-    protected Unit(String name){
+    protected Coordinates coordinates;
+    protected Unit(String name, int x, int y){
         this.name = name;
-
+        coordinates = new Coordinates(x,y);
     }
 
     public int move(){
@@ -24,5 +27,24 @@ public abstract class Unit implements GameInterface {
     public int attack(){
         return 1;
     }
+
+    public double[] getMinPosition(ArrayList<Unit> list){
+        double min = 1000;
+        double index =0;
+        for (int i = 0; i < list.size(); i++) {
+            double temp = Math.sqrt(
+                    (Math.pow(list.get(i).coordinates.x,2)-Math.pow(this.coordinates.x,2))+
+                    (Math.pow(list.get(i).coordinates.y,2)-Math.pow(this.coordinates.y,2))
+            );
+
+            if (temp<min){
+                index = i;
+                min = temp;
+            }
+        }
+        return new double[]{index, min} ;
+    }
+
+
 
 }
